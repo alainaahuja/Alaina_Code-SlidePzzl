b131599b688a71ff5e7d6aba615e0123f7e9f913
alainaahuja <59102537+alainaahuja@users.noreply.github.com>
2019-12-21 21:11:49 -0500

    gg

--- Slide_Puzzle.py
+++ Slide_Puzzle.py
@@ -44,7 +44,7 @@ def main():
     pygame.display.set_caption('Slide Puzzle')
     BASICFONT = pygame.font.Font('Chocolate-Drink.ttf', BASICFONTSIZE)
 
-    # Store the option buttons and their rectangles in OPTIONS.
+    # Store the option buttons and their rectangles in OPTIONS.ggg
     RESET_SURF, RESET_RECT = makeText('Reset',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
     NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
     SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
